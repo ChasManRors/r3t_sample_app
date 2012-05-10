9ba3b56c134873e9f9d9b7a62de4defbdce5ba5e
Charles Magid <cmmagid@gmail.com>
2012-05-10 11:56:30 -0400

    layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
 
-  get "static_pages/about"
 
-  get "static_pages/content"
+  # get "static_pages/home"
+  match '/home', to: 'static_pages#home'
 
-  get "static_pages/contact"
+  # get "static_pages/help"
+  match '/help', to: 'static_pages#help'
+
+  # get "static_pages/about"
+  match '/about', to: 'static_pages#about'
+
+  # get "static_pages/content"
+  match '/content', to: 'static_pages#content'
+
+  # get "static_pages/contact"
+  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -58,7 +66,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'static_pages#about'
+   root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
